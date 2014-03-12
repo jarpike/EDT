8ee5abc8b059c2da27f45b21439b5be3e2fa6eb3
Rémy F. <root@localhost>
2014-03-12 02:53:26 +0100

    better date
    TODO : better week view

--- js/DMController.js
+++ js/DMController.js
@@ -13,15 +13,15 @@ DMController={
 		var dms=JSON.parse(localStorage.DM||'[]');
 		if(!dms.length)return this.html("<h2>Aucun Devoirs Maison ... <small>Pour le moment</small></h2>");
 		function ctrl(pos){
-			return '<button type="button" onclick="DMController.done('+pos+',this)" class="btn btn-default"><span class="well glyphicon glyphicon glyphicon-trash"></span></button>';
+			return '<a onclick="DMController.done('+pos+',this)" class="btn btn-xs btn-default"><span class="glyphicon glyphicon glyphicon-trash"></span></a>';
 		}
 		this.html('<h2>Liste des DM :</h2>'+
 			'<table class="table table-condensed">'+
-			'<thead><tr><td>#</td><td>Titre</td><td>UE</td><td>Date</td><td>etat</td><td width="1%"></td></tr></thead>'+
+			'<thead><tr><td>#</td><td>Titre</td><td>UE</td><td>Date</td><td width="1%"></td></tr></thead>'+
 			'<tbody>'+
 			dms.map(function(dm,pos){if(!dm)return '';//previously deleted entry
 				return '<tr class="'+(dm.done?'active':['success','info','warning','danger'][dm.lv])+'"><td>'+
-					[pos,dm.title,dm.UE,(new Date(dm.date)).toLocaleDateString()+'',dm.desc,ctrl(pos)].join('</td><td>')+'</td></tr>';
+					[pos,dm.title,dm.UE,(new Date(dm.date)).toLocaleDateString(),ctrl(pos)].join('</td><td>')+'</td></tr>';
 			}).join('')+
 			'</tbody></table>');
 	}
